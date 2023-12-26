5c335bcbd15e4f4a70bf2fafbfd23708cfbfc22b
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-26 18:16:09 +0200

    another small change

--- barbershop_web/backend/server.js
+++ barbershop_web/backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 4000
-const HOST = 'http://ec2-51-20-131-65.eu-north-1.compute.amazonaws.com' // Update with your actual EC2 instance DNS name
+const HOST = 'ec2-51-20-131-65.eu-north-1.compute.amazonaws.com' // Update with your actual EC2 instance DNS name
 
 // Middleware
 app.use(
