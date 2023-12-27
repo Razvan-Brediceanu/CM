a3ce103dd37a9f4abf1ab2cf41d2edd2f841676f
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-27 15:01:20 +0200

    yeah more stuff

--- barbershop_web/functions/lambda.js
+++ barbershop_web/functions/lambda.js
@@ -1,4 +1,3 @@
-// lambda.js
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -6,6 +5,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+
 const connection = mongoose.connection
 
 connection.once('open', () => {
@@ -15,5 +15,17 @@ connection.once('open', () => {
 const app = require('../server')
 
 exports.handler = async function (event, context) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://geeks4life.netlify.app',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      },
+      body: '',
+    }
+  }
+
   return await app(event, context)
 }
