6647587b7ca6c0f2ae039cc2986c313bf9ded612
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-28 10:25:46 +0200

    testing one more thing

--- barbershop_web/src/components/LoginForm.jsx
+++ barbershop_web/src/components/LoginForm.jsx
@@ -34,7 +34,7 @@ const LoginForm = ({ setIsLoginPage }) => {
       }
 
       const response = await axios.post(
-        `${apiBaseURL}/login`, // Adjust the endpoint
+        `${apiBaseURL}/user/login`, // Adjust the endpoint
         loginData,
         {
           withCredentials: true,
