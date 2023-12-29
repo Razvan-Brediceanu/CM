777e7ddeb714a2a0311e56ec88b636e2bf058e99
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-29 13:05:09 +0200

    payment

--- barbershop_web/functions/create-payment.js
+++ barbershop_web/functions/create-payment.js
@@ -1,3 +1,5 @@
+// create-payment.js
+
 const stripe = require('stripe')(
   'sk_test_51OSbm5AMGDZssiK7U9w7grJ9DyAVKet0Dk4REGBA6fsNbvVp0J2juxxYlKCe3S8CEJcR2ccGN4fTkNi7sXWDzNfy00vh5QcLYA'
 )
@@ -6,11 +8,8 @@ exports.handler = async function (event, context) {
   try {
     console.log('Received request:', event)
 
-    const { amount } = JSON.parse(event.body)
-    console.log('Received amount:', amount)
-
     const paymentIntent = await stripe.paymentIntents.create({
-      amount,
+      amount: event.body.amount,
       currency: 'usd',
     })
 
@@ -22,6 +21,7 @@ exports.handler = async function (event, context) {
     }
   } catch (error) {
     console.error('Error creating payment intent:', error)
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' }),
