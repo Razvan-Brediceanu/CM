f9bbd5b141e1a8d97ac732d840ef4d0af7d091a6
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-29 12:59:22 +0200

    payment

--- barbershop_web/functions/create-payment.js
+++ barbershop_web/functions/create-payment.js
@@ -1,9 +1,16 @@
+const stripe = require('stripe')(
+  'sk_test_51OSbm5AMGDZssiK7U9w7grJ9DyAVKet0Dk4REGBA6fsNbvVp0J2juxxYlKCe3S8CEJcR2ccGN4fTkNi7sXWDzNfy00vh5QcLYA'
+)
+
 exports.handler = async function (event, context) {
   try {
     console.log('Received request:', event)
 
+    const { amount } = JSON.parse(event.body)
+    console.log('Received amount:', amount)
+
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: event.body.amount,
+      amount,
       currency: 'usd',
     })
 
