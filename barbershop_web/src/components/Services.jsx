f3956766bf44219d3d153e18c6846a1388fa6bb0
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-29 10:17:03 +0200

    yeah a few more stuff

--- barbershop_web/src/components/Services.jsx
+++ barbershop_web/src/components/Services.jsx
@@ -29,7 +29,7 @@ function ServicesComponent() {
       className='relative min-h-screen bg-cover'
       style={{ backgroundImage: `url(${backImage})` }}
     >
-      <div className='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container mx-auto px-4 lg:px-8 font-bold your-permanent-marker-text'>
+      <div className='absolute left-1/2 transform -translate-x-1/2 container mx-auto px-4 lg:px-8 font-bold your-permanent-marker-text sm:mt-15'>
         <h2 className='text-3xl font-bold mb-12 lg:mb-20 text-center'>
           Serviciile Noastre
         </h2>
