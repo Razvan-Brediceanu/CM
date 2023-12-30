c3b8697a3f635cc3edacd6868662ad07784520e4
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-30 17:46:57 +0200

    trying to fix profile routing

--- barbershop_web/src/components/UserProfile.jsx
+++ barbershop_web/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ import { jwtDecode as jwt_decode } from 'jwt-decode'
 import { useNavigate } from 'react-router-dom'
 import backImage from '../images/LoginRegister2.jpg'
 
-const apiBaseURL = '/.netlify/functions/server' // Adjust the base path
+const apiBaseURL = '/.netlify/functions/server/routes' // Adjust the base path
 
 const UserProfile = () => {
   const [userData, setUserData] = useState(null)
