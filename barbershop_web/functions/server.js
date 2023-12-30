03d5cdbb4eb51a448aff35a9809a37a4fe12eeb5
Razvan Brediceanu <56827474+Razvan-Brediceanu@users.noreply.github.com>
2023-12-30 17:57:33 +0200

    trying to fix profile routing

--- barbershop_web/functions/server.js
+++ barbershop_web/functions/server.js
@@ -17,8 +17,8 @@ app.use(express.json())
 const userRouter = require('./routes/users')
 const refreshTokenRouter = require('./routes/refreshToken')
 
-app.use('/.netlify/functions/server/routes/user', userRouter) // Adjust the base path
-app.use('/.netlify/functions/server/routes/refresh', refreshTokenRouter) // Adjust the base path
+app.use('/.netlify/functions/server/user', userRouter) // Adjust the base path
+app.use('/.netlify/functions/server/refresh', refreshTokenRouter) // Adjust the base path
 
 // Connect to MongoDB
 mongoose
